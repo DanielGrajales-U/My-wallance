53831d11a37afc057c0e960cad6505cdde0ab68f
DanielGrajales-U <dani22.24gra@gmail.com>
2024-04-23 16:56:36 -0500

    add importmetaenv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv{
+    readonly VITE_BACKEND_URL: string;
+    readonly VITE_VALIDATION_ERROR: string
+    readonly VITE_INVALID_DATA: string
+    readonly VITE_DATA_ALREADY_EXISTS: string
+}
